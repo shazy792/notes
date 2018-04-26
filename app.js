1845964045001d27f9cca76f826ab7aa9579a270
Shahzil Sheikh <shazy792@hotmail.com>
2018-04-26 02:26:24 -0500

    serving static files in public folder

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/note/:id', (req, res) => {
